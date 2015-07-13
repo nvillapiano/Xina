bf6ace13e201767a8606a3bfecf394f9a7d315e1
nvillapiano <nvillapiano@gmail.com>
2015-07-12 20:48:16 -0400

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Smooth Scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+// Toggle Form
+$(".book").click(function(){
+    $(".event-form").addClass('slide');
+});
+
+var hash = false; 
+checkHash();
+
+function checkHash(){ 
+	if(window.location.hash != hash) { 
+		hash = window.location.hash; 
+		processHash(hash); 
+	}
+	t=setTimeout("checkHash()", 400); 
+}
+
+function processHash(hash){
+    $(".event-form").addClass('slide');
+    $('html,body').animate({
+   		scrollTop: $("#event-form").offset().top
+	});
+}
